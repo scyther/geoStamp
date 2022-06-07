c64b69ae52beb7df86d77a477e665dcbd730bc0d
Chirag Sharma <sharma.chirag3737@gmail.com>
2022-06-07 15:40:09 +0530

    structure finalised

---
+++ __main__.py
@@ -0,0 +1,12 @@
+print("____-menu_____")
+print("1: To add to Context Menu \n\
+2: to add GeoStamp to Image")
+  
+ch = int(input())
+  
+if ch == 1:
+    print("option 1")
+  
+if ch == 2:
+    print("option 2")
+  
